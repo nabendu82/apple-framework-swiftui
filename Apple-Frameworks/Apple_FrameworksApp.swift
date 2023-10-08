1f6b494c8f3e3eb37ea36ba7a9175ec99468d8ff
nabendu82 <nabendu.biswas@gmail.com>
2023-10-08 09:52:09 +0530

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Nabendu Biswas on 08/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
